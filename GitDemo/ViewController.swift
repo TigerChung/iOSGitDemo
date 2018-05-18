523276be843120847518e042c7746254dad38785
Kun Tai Chung <365tiger@gmail.com>
2018-05-18 11:00:23 +0800

    test modified

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let msg = " Hello Git"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(msg)
     }
 
     override func didReceiveMemoryWarning() {
